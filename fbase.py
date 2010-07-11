cdd06fac7a630faeb708382ba2ce4948ebe1c3c1
dan <dan@thule.(none)>
2010-07-11 22:31:58 +0200

    Add freebase utils

---
+++ fbase.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from freebase.api import HTTPMetawebSession, MetawebError
+
+session = HTTPMetawebSession('www.freebase.com')
+
+def listValues(freebase_type, values = ('name'), limit = 100):
+    """Select certain properties from all members of a particular category
+    on freebase"""
+    querydict = {'type' : freebase_type, 'limit' : limit}
+    for value in values:
+        querydict[value] = None
+    results = []
+    for item in session.mqlread([querydict]):
+        row = dict((k,v) for k,v in item.iteritems() if k in values)
+        results.append(row)
+    return results
+
